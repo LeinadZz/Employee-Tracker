358e1fe166efc417e63eb772e14beac6b751eb91
Daniel <leinad6ix.z@gmail.com>
2022-11-04 06:15:45 -0700

    Created query function to find employee by manager

--- db/index.js
+++ db/index.js
@@ -24,4 +24,11 @@ class DB {
     );
   }
 
+  findAllEmployeesByManager(managerId) {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+      managerId
+    );
+  }
+
 }
