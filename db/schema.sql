88936dfe04a3978a4dbbc620377b186096aa565b
Daniel <leinad6ix.z@gmail.com>
2022-11-04 05:18:45 -0700

    Created new table for db for employee's role's

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
