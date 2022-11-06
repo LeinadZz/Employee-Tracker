23c0520630c6f3b3316fc2ad802274b7d812300c
Daniel <leinad6ix.z@gmail.com>
2022-11-06 01:21:26 -0800

    update

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Zuko34461",
+  password: 'Wdb7Gf2:HSPHt7m',
   database: "employees"
 });
 
